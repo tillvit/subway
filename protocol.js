91d20cf1b82e631a08870eebf8b9985b2536f413
Kevin Yu <kevinyu567@gmail.com>
2025-12-17 21:37:07 -0500

    Reformat :)

--- protocol.js
+++ protocol.js
@@ -1,245 +1,245 @@
 export const MessageType = {
-  AUTH: 0,
-  CREATE_CONNECTION: 1,
-  CLOSE_CONNECTION: 2,
-  DATA: 3,
+    AUTH: 0,
+    CREATE_CONNECTION: 1,
+    CLOSE_CONNECTION: 2,
+    DATA: 3,
 }
 
 const writeULEB128 = (value) => {
-  const bytes = []
-  do {
-    let byte = value & 0x7F
-    value >>= 7
-    if (value !== 0) {
-      byte |= 0x80
-    }
-    bytes.push(byte)
-  } while (value !== 0)
-  return Buffer.from(bytes)
+    const bytes = []
+    do {
+        let byte = value & 0x7F
+        value >>= 7
+        if (value !== 0) {
+            byte |= 0x80
+        }
+        bytes.push(byte)
+    } while (value !== 0)
+    return Buffer.from(bytes)
 }
 
 const readShort = (bytes) => {
-  return (bytes[0] << 8) | bytes[1]
+    return (bytes[0] << 8) | bytes[1]
 }
 
 class Lock {
-  acquired = false;
-  queue = [];
+    acquired = false;
+    queue = [];
 
-  async acquire() {
-    if (!this.acquired) {
-      this.acquired = true;
-    } else {
-      return new Promise((resolve, _) => {
-        this.queue.push(resolve);
-      });
-    }
-  }
-
-  async release() {
-    if (this.queue.length === 0 && this.acquired) {
-      this.acquired = false;
-      return;
+    async acquire() {
+        if (!this.acquired) {
+            this.acquired = true;
+        } else {
+            return new Promise((resolve, _) => {
+                this.queue.push(resolve);
+            });
+        }
     }
 
-    const continuation = this.queue.shift();
-    return new Promise((res) => {
-      continuation();
-      res();
-    });
-  }
+    async release() {
+        if (this.queue.length === 0 && this.acquired) {
+            this.acquired = false;
+            return;
+        }
+
+        const continuation = this.queue.shift();
+        return new Promise((res) => {
+            continuation();
+            res();
+        });
+    }
 }
 
 export class RingBuffer {
-  constructor(size) {
-    this.buffer = Buffer.alloc(size)
-    this.size = size
-    this.start = 0
-    this.end = 0
-  }
-
-  get length() {
-    if (this.end >= this.start) {
-      return this.end - this.start
-    } else {
-      return this.size - this.start + this.end
+    constructor(size) {
+        this.buffer = Buffer.alloc(size)
+        this.size = size
+        this.start = 0
+        this.end = 0
     }
-  }
 
-  async push(data) {
-    for (let i = 0; i < data.length; i++) {
-      this.buffer[this.end] = data[i]
-      this.end = (this.end + 1) % this.size
+    get length() {
+        if (this.end >= this.start) {
+            return this.end - this.start
+        } else {
+            return this.size - this.start + this.end
+        }
     }
-  }
 
-  read(length) {
-    if (length > this.length) {
-      length = this.length
+    push(data) {
+        for (let i = 0; i < data.length; i++) {
+            this.buffer[this.end] = data[i]
+            this.end = (this.end + 1) % this.size
+        }
     }
-    const result = Buffer.alloc(length)
-    for (let i = 0; i < length; i++) {
-      result[i] = this.buffer[(this.start + i) % this.size]
+
+    read(length) {
+        if (length > this.length) {
+            length = this.length
+        }
+        const result = Buffer.alloc(length)
+        for (let i = 0; i < length; i++) {
+            result[i] = this.buffer[(this.start + i) % this.size]
+        }
+        return result
     }
-    return result
-  }
 
-  readRange(start, end) {
-    const result = Buffer.alloc(end - start)
-    for (let i = start; i < end; i++) {
-      result[i - start] = this.buffer[(this.start + i) % this.size]
+    readRange(start, end) {
+        const result = Buffer.alloc(end - start)
+        for (let i = start; i < end; i++) {
+            result[i - start] = this.buffer[(this.start + i) % this.size]
+        }
+        return result
     }
-    return result
-  }
 
-  readULEB128(offset) {
-    let result = 0
-    let shift = 0
-    let byte
-    let bytesRead = 0
+    readULEB128(offset) {
+        let result = 0
+        let shift = 0
+        let byte
+        let bytesRead = 0
 
-    do {
-      byte = this.buffer[(this.start + offset + bytesRead) % this.size]
-      result |= (byte & 0x7F) << shift
-      shift += 7
-      bytesRead++
-    } while (byte & 0x80)
+        do {
+            byte = this.buffer[(this.start + offset + bytesRead) % this.size]
+            result |= (byte & 0x7F) << shift
+            shift += 7
+            bytesRead++
+        } while (byte & 0x80)
 
-    return { value: result, length: bytesRead }
-  }
+        return {value: result, length: bytesRead}
+    }
 
-  removeStart(length) {
-    this.start = (this.start + length) % this.size
-  }
+    removeStart(length) {
+        this.start = (this.start + length) % this.size
+    }
 
-  clear() {
-    this.start = 0
-    this.end = 0
-  }
+    clear() {
+        this.start = 0
+        this.end = 0
+    }
 }
 
 export class ConnectionHandler {
-  constructor(onPacket) {
-    this.buffer = new RingBuffer(1048576)
-    this.onPacket = onPacket
-    this.lock = new Lock()
-  }
-
-  reset() {
-    this.buffer.clear()
-  }
-
-  async push(data) {
-    await this.lock.acquire();
-    this.buffer.push(data)
-    this._parse()
-    this.lock.release();
-  }
-
-  _parse() {
-    while (this.buffer.length > 0) {
-      const messageType = this.buffer.read(1)[0]
-      switch (messageType) {
-        case MessageType.AUTH: {
-          const AUTH_TOKEN_LENGTH = 16
-          if (this.buffer.length < 1 + AUTH_TOKEN_LENGTH) {
-            return
-          }
-          const authToken = this.buffer.readRange(1, 1 + AUTH_TOKEN_LENGTH)
-          this.onPacket?.({ type: MessageType.AUTH, authToken })
-          this.buffer.removeStart(1 + AUTH_TOKEN_LENGTH)
-          break
-        }
-        case MessageType.CREATE_CONNECTION: {
-          if (this.buffer.length < 3) {
-            return
-          }
-          const mapId = readShort(this.buffer.readRange(1, 3))
-          this.onPacket?.({ type: MessageType.CREATE_CONNECTION, mapId })
-          this.buffer.removeStart(3)
-          break
-        }
-        case MessageType.CLOSE_CONNECTION: {
-          if (this.buffer.length < 3) {
-            return
-          }
-          const mapId = readShort(this.buffer.readRange(1, 3))
-          this.onPacket?.({ type: MessageType.CLOSE_CONNECTION, mapId })
-          this.buffer.removeStart(3)
-          break
-        }
-        case MessageType.DATA: {
-          if (this.buffer.length < 3) {
-            return
-          }
-          const mapId = readShort(this.buffer.readRange(1, 3))
-          let { value: length, length: lengthBytes } = this.buffer.readULEB128(3)
-          if (this.buffer.length < 3 + lengthBytes + length) {
-            return
-          }
-          const payload = Buffer.from(this.buffer.readRange(3 + lengthBytes, 3 + lengthBytes + length))
-          this.onPacket?.({ type: MessageType.DATA, mapId, payload })
-          this.buffer.removeStart(3 + lengthBytes + length)
-          break
-        }
-      }
+    constructor(onPacket) {
+        this.buffer = new RingBuffer(1048576)
+        this.onPacket = onPacket
+        this.lock = new Lock()
     }
-  }
+
+    reset() {
+        this.buffer.clear()
+    }
+
+    async push(data) {
+        await this.lock.acquire();
+        this.buffer.push(data)
+        this._parse()
+        this.lock.release();
+    }
+
+    _parse() {
+        while (this.buffer.length > 0) {
+            const messageType = this.buffer.read(1)[0]
+            switch (messageType) {
+                case MessageType.AUTH: {
+                    const AUTH_TOKEN_LENGTH = 16
+                    if (this.buffer.length < 1 + AUTH_TOKEN_LENGTH) {
+                        return
+                    }
+                    const authToken = this.buffer.readRange(1, 1 + AUTH_TOKEN_LENGTH)
+                    this.onPacket?.({type: MessageType.AUTH, authToken})
+                    this.buffer.removeStart(1 + AUTH_TOKEN_LENGTH)
+                    break
+                }
+                case MessageType.CREATE_CONNECTION: {
+                    if (this.buffer.length < 3) {
+                        return
+                    }
+                    const mapId = readShort(this.buffer.readRange(1, 3))
+                    this.onPacket?.({type: MessageType.CREATE_CONNECTION, mapId})
+                    this.buffer.removeStart(3)
+                    break
+                }
+                case MessageType.CLOSE_CONNECTION: {
+                    if (this.buffer.length < 3) {
+                        return
+                    }
+                    const mapId = readShort(this.buffer.readRange(1, 3))
+                    this.onPacket?.({type: MessageType.CLOSE_CONNECTION, mapId})
+                    this.buffer.removeStart(3)
+                    break
+                }
+                case MessageType.DATA: {
+                    if (this.buffer.length < 3) {
+                        return
+                    }
+                    const mapId = readShort(this.buffer.readRange(1, 3))
+                    let {value: length, length: lengthBytes} = this.buffer.readULEB128(3)
+                    if (this.buffer.length < 3 + lengthBytes + length) {
+                        return
+                    }
+                    const payload = Buffer.from(this.buffer.readRange(3 + lengthBytes, 3 + lengthBytes + length))
+                    this.onPacket?.({type: MessageType.DATA, mapId, payload})
+                    this.buffer.removeStart(3 + lengthBytes + length)
+                    break
+                }
+            }
+        }
+    }
 }
 
-export function Serialize(message) {
-  switch (message.type) {
-    case MessageType.AUTH: {
-      const authBuffer = Buffer.from(message.authToken)
-      return Buffer.concat([Buffer.from([MessageType.AUTH]), authBuffer])
+export function serialize(message) {
+    switch (message.type) {
+        case MessageType.AUTH: {
+            const authBuffer = Buffer.from(message.authToken)
+            return Buffer.concat([Buffer.from([MessageType.AUTH]), authBuffer])
+        }
+        case MessageType.CREATE_CONNECTION: {
+            return Buffer.from([MessageType.CREATE_CONNECTION, (message.mapId >> 8) & 0xFF, message.mapId & 0xFF])
+        }
+        case MessageType.CLOSE_CONNECTION: {
+            return Buffer.from([MessageType.CLOSE_CONNECTION, (message.mapId >> 8) & 0xFF, message.mapId & 0xFF])
+        }
+        case MessageType.DATA: {
+            const header = Buffer.from([MessageType.DATA, (message.mapId >> 8) & 0xFF, message.mapId & 0xFF])
+            const length = writeULEB128(message.payload.byteLength)
+            return Buffer.concat([header, length, Buffer.from(message.payload)])
+        }
     }
-    case MessageType.CREATE_CONNECTION: {
-      const header = Buffer.from([MessageType.CREATE_CONNECTION, (message.mapId >> 8) & 0xFF, message.mapId & 0xFF])
-      return header
-    }
-    case MessageType.CLOSE_CONNECTION: {
-      const header = Buffer.from([MessageType.CLOSE_CONNECTION, (message.mapId >> 8) & 0xFF, message.mapId & 0xFF])
-      return header
-    }
-    case MessageType.DATA: {
-      const header = Buffer.from([MessageType.DATA, (message.mapId >> 8) & 0xFF, message.mapId & 0xFF])
-      const length = writeULEB128(message.payload.byteLength)
-      return Buffer.concat([header, length, Buffer.from(message.payload)])
-    }
-  }
 }
 
 export class ConnectionMap {
-  constructor() {
-    this.map = new Map()
-  }
-  assign(socket, mapId) {
-    if (mapId !== undefined && this.map.has(mapId)) {
-      throw new Error(`Map ID ${mapId} is already in use`)
-    } else if (mapId !== undefined) {
-      this.map.set(mapId, socket)
-      return mapId
+    constructor() {
+        this.map = new Map()
     }
-    mapId = Math.floor(Math.random() * 0xFFFF)
-    while (this.map.has(mapId)) {
-      mapId = Math.floor(Math.random() * 0xFFFF)
-    }
-    this.map.set(mapId, socket)
-    return mapId
-  }
 
-
-  get(mapId) {
-    return this.map.get(mapId)
-  }
-  close(mapId) {
-    this.map.get(mapId)?.destroy()
-    this.map.delete(mapId)
-  }
-  clear() {
-    for (const socket of this.map.values()) {
-      socket.destroy()
+    assign(socket, mapId) {
+        if (mapId !== undefined && this.map.has(mapId)) {
+            throw new Error(`Map ID ${mapId} is already in use`)
+        } else if (mapId !== undefined) {
+            this.map.set(mapId, socket)
+            return mapId
+        }
+        mapId = Math.floor(Math.random() * 0xFFFF)
+        while (this.map.has(mapId)) {
+            mapId = Math.floor(Math.random() * 0xFFFF)
+        }
+        this.map.set(mapId, socket)
+        return mapId
     }
-    this.map.clear()
-  }
+
+    get(mapId) {
+        return this.map.get(mapId)
+    }
+
+    close(mapId) {
+        this.map.get(mapId)?.destroy()
+        this.map.delete(mapId)
+    }
+
+    clear() {
+        for (const socket of this.map.values()) {
+            socket.destroy()
+        }
+        this.map.clear()
+    }
 }
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 import net from "net";
-import { ConnectionHandler, ConnectionMap, MessageType, Serialize } from "./protocol.js";
+import { ConnectionHandler, ConnectionMap, MessageType, serialize } from "./protocol.js";
 
 const EXPOSE_PORT = 8199
 const REMOTE_PORT = 8198
@@ -10,123 +10,122 @@ let remoteSocket = null
 
 const connectionMap = new ConnectionMap()
 
-function buf2hex(buffer) { // buffer is an ArrayBuffer
-  return [...new Uint8Array(buffer)]
-      .map(x => x.toString(16).padStart(2, '0'))
-      .join('');
+function buf2hex(buffer) {
+    return [...new Uint8Array(buffer)]
+        .map(x => x.toString(16).padStart(2, '0'))
+        .join('');
 }
 
 const exposeServer = net.createServer((socket) => {
-  if (!remoteSocket) {
-    socket.destroy()
-    return
-  }
-  console.log(`Client connected to expose server from ${socket.remoteAddress}:${socket.remotePort}`)
-  const mapId = connectionMap.assign(socket)
+    if (!remoteSocket) {
+        socket.destroy()
+        return
+    }
+    console.log(`Client connected to expose server from ${socket.remoteAddress}:${socket.remotePort}`)
+    const mapId = connectionMap.assign(socket)
 
-  remoteSocket.write(Serialize({
-    type: MessageType.CREATE_CONNECTION,
-    mapId: mapId
-  }))
+    remoteSocket.write(serialize({
+        type: MessageType.CREATE_CONNECTION,
+        mapId: mapId
+    }))
 
-  socket.on("close", () => {
-    connectionMap.close(mapId)
-    // Type 2 = close connection
-    remoteSocket?.write(Serialize({
-      type: MessageType.CLOSE_CONNECTION,
-      mapId: mapId
-    }))
-    console.log(`Expose client disconnected for mapId ${mapId}`)
-  })
+    socket.on("close", () => {
+        connectionMap.close(mapId)
+        remoteSocket?.write(serialize({
+            type: MessageType.CLOSE_CONNECTION,
+            mapId: mapId
+        }))
+        console.log(`Expose client disconnected for mapId ${mapId}`)
+    })
 
-  socket.on("data", (data) => {
-    console.log(`Received from client: `, data)
-    remoteSocket?.write(Serialize({
-      type: MessageType.DATA,
-      mapId: mapId,
-      payload: data
-    }))
-  })
-  socket.on("error", (err) => {
-    console.error("Expose socket error:", err)
-    connectionMap.close(mapId)
-    remoteSocket?.write(Serialize({
-      type: MessageType.CLOSE_CONNECTION,
-      mapId: mapId
-    }))
-  })
+    socket.on("data", (data) => {
+        console.log(`Received from client: `, data)
+        remoteSocket?.write(serialize({
+            type: MessageType.DATA,
+            mapId: mapId,
+            payload: data
+        }))
+    })
+    socket.on("error", (err) => {
+        console.error("Expose socket error:", err)
+        connectionMap.close(mapId)
+        remoteSocket?.write(serialize({
+            type: MessageType.CLOSE_CONNECTION,
+            mapId: mapId
+        }))
+    })
 })
 
 
 const remoteServer = net.createServer((socket) => {
-  console.log(`Client connected to remote server from ${socket.remoteAddress}:${socket.remotePort}`)
+    console.log(`Client connected to remote server from ${socket.remoteAddress}:${socket.remotePort}`)
 
-  if (!remoteSocket) {
-    remoteSocket = socket
-  }
-  let authenticated = false
-  const authTimeout = setTimeout(() => {
-    console.log("Client failed to authenticate in time. Disconnecting.")
-    socket.destroy()
-  }, 10000) // 10 seconds to authenticate
-
-  const parser = new ConnectionHandler(onPacket)
-
-  const parserDebugInterval = setInterval(() => {
-    console.log(parser.buffer.length)
-    console.log(parser.buffer.read(parser.buffer.length))
-  }, 1000);
-
-  function onPacket(packet) {
-    switch (packet.type) {
-      case MessageType.AUTH: // AUTHENTICATE
-        const receivedToken = buf2hex(packet.authToken)
-        if (receivedToken === auth) {
-          authenticated = true
-          clearTimeout(authTimeout)
-          console.log("Client authenticated successfully.")
-        } else {
-          console.warn("Client failed to authenticate. Disconnecting.")
-          socket.destroy()
-        }
-        break
-      case MessageType.DATA: // DATA
-        if (!authenticated) {
-          console.warn("Received data before authentication. Ignoring.")
-          socket.destroy()
-        }
-        const clientSocket = connectionMap.get(packet.mapId)
-        if (clientSocket) {
-          clientSocket.write(packet.payload)
-        }
-        break
-      case MessageType.CLOSE_CONNECTION: // CLOSE_CONNECTION
-        if (!authenticated) {
-          console.warn("Received data before authentication. Ignoring.")
-          socket.destroy()
-        }
-        connectionMap.close(packet.mapId)
-        break
+    if (!remoteSocket) {
+        remoteSocket = socket;
     }
-  }
+    let authenticated = false;
+    const authTimeout = setTimeout(() => {
+        console.log("Client failed to authenticate in time. Disconnecting.")
+        socket.destroy()
+    }, 10000) // 10 seconds to authenticate
 
-  socket.on("data", (data) => {
-    parser.push(data);
-  })
+    const parser = new ConnectionHandler(onPacket)
 
-  socket.on("close", () => {
-    if (socket === remoteSocket) {
-      remoteSocket = null
-      connectionMap.clear()
-      console.warn(`Remote socket disconnected.`)
+    const parserDebugInterval = setInterval(() => {
+        console.log(parser.buffer.length)
+        console.log(parser.buffer.read(parser.buffer.length))
+    }, 1000);
+
+    function onPacket(packet) {
+        switch (packet.type) {
+            case MessageType.AUTH: // AUTHENTICATE
+                const receivedToken = buf2hex(packet.authToken)
+                if (receivedToken === auth) {
+                    authenticated = true;
+                    clearTimeout(authTimeout);
+                    console.log("Client authenticated successfully.");
+                } else {
+                    console.warn("Client failed to authenticate. Disconnecting.");
+                    socket.destroy();
+                }
+                break
+
+            case MessageType.DATA: // DATA
+                if (!authenticated) {
+                    console.warn("Received data before authentication. Ignoring.");
+                    socket.destroy();
+                }
+                const clientSocket = connectionMap.get(packet.mapId);
+                clientSocket?.write(packet.payload)
+                break;
+
+            case MessageType.CLOSE_CONNECTION: // CLOSE_CONNECTION
+                if (!authenticated) {
+                    console.warn("Received data before authentication. Ignoring.")
+                    socket.destroy()
+                }
+                connectionMap.close(packet.mapId)
+                break
+        }
     }
-    clearInterval(parserDebugInterval);
-  })
+
+    socket.on("data", (data) => {
+        void parser.push(data);
+    })
+
+    socket.on("close", () => {
+        if (socket === remoteSocket) {
+            remoteSocket = null;
+            connectionMap.clear();
+            console.warn(`Remote socket disconnected.`);
+        }
+        clearInterval(parserDebugInterval);
+    })
 })
 
 exposeServer.listen(EXPOSE_PORT, () => {
-  console.log(`Expose server listening on port ${EXPOSE_PORT}`)
+    console.log(`Expose server listening on port ${EXPOSE_PORT}`)
 })
 remoteServer.listen(REMOTE_PORT, () => {
-  console.log(`Remote server listening on port ${REMOTE_PORT}`)
+    console.log(`Remote server listening on port ${REMOTE_PORT}`)
 })
